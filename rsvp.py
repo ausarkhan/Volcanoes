d56cae5e869a954d5d12b9c30c799af307f86840
Janya ward <janyaward33@gmail.com>
2025-11-25 16:11:53 -0600

    Create RSVP class and initialize attributes

---
+++ rsvp.py
@@ -0,0 +1,7 @@
+class RSVP:
+    def __init__(self, event, user_id, role):
+        self.event = event
+        self.user_id = user_id
+        self.role = role
+        self.created_at = None
+        self.status = "going"
